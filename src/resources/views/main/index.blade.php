aff06f145929a6423f9ad4a4b966168a5018ebdf
Hideyuki <hide27yuki@gmail.com>
2025-03-20 07:47:49 +0900

    [Backup]feature2/module1

--- src/resources/views/main/index.blade.php
+++ src/resources/views/main/index.blade.php
@@ -35,7 +35,6 @@
         <p>URL</p>
         <a href="{{ url('/url') }}">Check URL</a>
     </div>
-</div>
 
 <!-- アプリインストール用ボタン -->
 <button id="installBtn" style="display: none;">インストール！</button>
@@ -97,7 +96,7 @@
         document.getElementById('quote-header').textContent = quotes[randomIndex];
     }
 
-    // 名言を取得して、5秒ごとに更新
+    // 名言を取得して、8秒ごとに更新
     fetchQuotes().then(quotes => {
         // 初回表示
         displayRandomQuote(quotes);
@@ -107,5 +106,7 @@
         }, 8000);
     });
 </script>
+</div>
+</div>
 </body>
 </html>

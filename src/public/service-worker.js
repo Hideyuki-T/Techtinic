721369a59b40c414b06b8e7950e9c5a46ab9a298
Hideyuki <hide27yuki@gmail.com>
2025-03-20 16:57:45 +0900

    [add]テトリス | オフラインで使用可能

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -4,17 +4,20 @@ const urlsToCache = [
     '/main',                    // メインページ
     '/chat',                    // チャットページ
     '/chat/indexedDBUtil.js',   // チャット用JS
-    '/chat-data-view',          // チャットデータ表示ページ
     '/game',                    // ゲームトップページ
-    '/tetris',                  // テトリスページ
     '/ec',                      // ECサイトページ
     '/url',                     // お気に入り用ページ
     '/css/mainPageStyles.css',  // CSS
     '/quotes.json',             // 名言リストJSON
     '/images/icon_192x192.png', // アイコン
     '/images/icon_512x512.png', // アイコン
-    '/api/chat/messages',       // API：チャットメッセージ取得（GET）
-    '/api/chat/data'            // API：チャットデータ取得（GET）
+    '/tetris',                  // テトリスページ
+    '/js/tetris/main.js',
+    '/js/tetris/score.js',
+    '/js/tetris/piece.js',
+    '/js/tetris/game.js',
+    '/js/tetris/renderer.js',
+    '/js/tetris/board.js',
 ];
 
 self.addEventListener('install', event => {
